8baf4646aa36ef8f9a75067cae7ff02b7636acf4
Leonardo Resende Corrêa Lima <leonardoresendelima@gmail.com>
2018-08-13 16:06:45 -0300

    Hiding and showing components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,16 +7,26 @@ import { Recipe } from '../recipe.model';
 	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+	@Output() recipeWasSelected = new EventEmitter<Recipe>();
+
 	recipes: Recipe[] = [
 		new Recipe(
 			"A Test Recipe",
 			"This is simply a test",
-			"https://www.bbcgoodfood.com/sites/default/files/recipe_images/recipe-image-legacy-id--1259679_8.jpg")
+			"https://www.bbcgoodfood.com/sites/default/files/recipe_images/recipe-image-legacy-id--1259679_8.jpg"),
+		new Recipe(
+			"A Fucking Nice Test Recipe",
+			"This is simply a fucking test",
+			"https://www.seriouseats.com/recipes/images/2016/08/20160713-mixed-fruit-pie-vicky-wasik-12-1500x1125.jpg")
 	];
 
 	constructor() { }
 
 	ngOnInit() {
+	}
+
+	onRecipeSelected(recipe : Recipe){
+		this.recipeWasSelected.emit(recipe);
 	}
 
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+	selector: 'app-shopping-edit',
+	templateUrl: './shopping-edit.component.html',
+	styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+	@ViewChild('nameInput') nameInputRef: ElementRef;
+	@ViewChild('amountInput') amountInputRef: ElementRef;
 
-  constructor() { }
+	@Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  ngOnInit() {
-  }
+	constructor() { }
 
+	ngOnInit() {
+	}
+
+	onAddItem() {
+		const newIngredient = new Ingredient(
+			this.nameInputRef.nativeElement.value,
+			this.amountInputRef.nativeElement.value
+		)
+		this.ingredientAdded.emit(newIngredient);
+	}
 }
